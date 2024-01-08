ff2101e3788c0909675b68521c86022eb27b51d0
Pharmas67 <m.gurrath62@gmail.com>
2024-01-09 00:37:26 +0100

    9 jan

--- src/main/java/main/service/UserServiceImpl.java
+++ src/main/java/main/service/UserServiceImpl.java
@@ -7,12 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import main.Repositories.UserRepository;
 import main.model.User;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.data.relational.core.query.CriteriaDefinition;
+import org.springframework.data.relational.core.query.Query;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
+
+import static org.springframework.data.relational.core.query.Criteria.where;
 
 @Service
 @RequiredArgsConstructor
@@ -26,34 +30,69 @@ public class UserServiceImpl implements UserService{
     public Mono<User> createUser(User user) {
         user.setId(UUID.randomUUID());
 
+        if(userExists(user.getEmail())){
+            System.out.println("email already taken");
+            return null;
+        }
+
         var r2Template = new R2dbcEntityTemplate(conn);
         return r2Template.insert(user);
+    }
 
+    private boolean userExists(String email){
+        var r2Template = new R2dbcEntityTemplate(conn);
+
+        CriteriaDefinition criteria = where("email").is(email);
+        Query query = Query.query(criteria);
+        Mono cEmail = null;
+        cEmail = r2Template.selectOne(query, Mono.class);
+        cEmail.subscribe(
+                entity-> {
+                    System.out.println(entity);
+                }
+        );
+        return false;
     }
 
     @Override
     public Mono<User> updateUser(Jwt jwt) {
-
-        var payload = jwt.getClaims();
-
-        jwt.getClaimAsString()
-
-        User user = (User) payload.get("email");
-
         var r2Template = new R2dbcEntityTemplate(conn);
-        return r2Template.update(user);
+
+        User user = new User();
+        user.setEmail(jwt.getClaimAsString("email"));
+        user.setId(jwt.getClaim("id"));
+        user.setName(jwt.getClaimAsString("name"));
+
+        if(!userExists(user.getEmail())){
+            System.out.println("User not found or false request");
+            return null;
+        } else {
+            boolean result = false;
+            if(result == Boolean.TRUE.equals(repo.existsById(user.getId()).block())){
+                System.out.println("User not found or something went wrong");
+                return null;
+            }
+        }
+        return repo.save(user);
     }
 
     @Override
     public Mono<Void> deleteUserById(Jwt jwt) {
-
-        var payload = jwt.getClaims();
-
-        User user = (User) payload.get("email");
-
-        //UUID id = (UUID) user.id;
         var r2Template = new R2dbcEntityTemplate(conn);
-        r2Template.delete(user);
-        return null;
+
+        String email = jwt.getClaimAsString("email");
+        UUID id = jwt.getClaim("id");
+
+        if(!userExists(email)){
+            System.out.println("User not found or false request");
+            return null;
+        } else {
+            boolean result = false;
+            if(result == Boolean.TRUE.equals(repo.existsById(id).block())){
+                System.out.println("User not found or something went wrong");
+                return null;
+            }
+        }
+        return repo.deleteById(id);
     }
 }
